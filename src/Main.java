be5210486ab694d32d2a94bcfb03e34663d311dc
Redd2019 <anton-mikerin@yandex.ru>
2023-02-03 07:07:50 +0300

    Выполнено задание 1 и начато задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        printLeapYear(2039);
+        task2();
 
     }
 
-    public static void task1() {
-        System.out.println("Задача 1");
+       public static void printLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год - вискосный год.");
+        }else {
+            System.out.println(year + " год -  не високосный год.");
+        }
+        }
 
+        public static void task2() {
+            System.out.println("Задача 2");
+            int clientOs = 0;
+            int clientDeviceYear = 2015;
+            if (clientDeviceYear <= 2015) {
+                if (clientOs == 1) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+            } else if (clientDeviceYear > 2015) {
+                if (clientOs == 1){
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                if (clientOs==0){
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+            }
+        }
     }
-}
